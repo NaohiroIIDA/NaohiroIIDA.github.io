6506cd9f5d18c881aa14627bbc4026619b6b4f99
Naohiro IIDA <nao@nekotorobot.com>
2015-10-12 18:10:56 +0900

    send

--- script2.js
+++ script2.js
@@ -7,11 +7,11 @@ function kakunin(btnNo){
 
   
   if (btnNo == 1){
-   pkStatusStore.push({ 'status' : 'push1!' });
+   pkStatusStore.send({ 'status' : 'push1!' });
   }
 
   if (btnNo == 2){
-   pkStatusStore.push({ 'status' : 'push2!' });
+   pkStatusStore.send({ 'push2!' });
   }
 
 
