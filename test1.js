5873024da2cc64de57076bc7b2d34db37b15205a
Naohiro IIDA <nao@nekotorobot.com>
2015-10-12 12:39:49 +0900

    2

--- test1.js
+++ test1.js
@@ -3,19 +3,6 @@ var milkcocoa = new MilkCocoa("leadida6ji33.mlkcca.com");
 var pkStatusStore = milkcocoa.dataStore('test');
 
 
-function kakunin(btnNo){
-
-  
-  if (btnNo == 1){
    pkStatusStore.push({ 'status' : 'push1!' });
-  }
-
-  if (btnNo == 2){
-   pkStatusStore.push({ 'status' : 'push2!' });
-  }
 
 
-
-  
-  
-}
