e9fa20d9d6e05cc40b8cec1d927c5c2b956ffc19
Naohiro IIDA <nao@nekotorobot.com>
2017-04-29 05:26:10 +0900

    Add files via upload

---
+++ junction.js
@@ -0,0 +1,24 @@
+
+var milkcocoa = new MilkCocoa('applej226hj6o.mlkcca.com');
+var StatusStore = milkcocoa.dataStore('esp8266');
+
+
+function command_send(btnNo){
+
+  
+  if (btnNo == 1){
+   StatusStore.send({ 'cmd' : 'R/' });
+  }
+
+  if (btnNo == 2){
+   StatusStore.send({ 'cmd' : 's/' });
+  }
+
+  if (btnNo == 3){
+   StatusStore.send({ 'cmd' : 'q/' });
+  }
+   if (btnNo == 4){
+   StatusStore.send({ 'cmd' : 'P180/' });
+  }  
+  
+}
