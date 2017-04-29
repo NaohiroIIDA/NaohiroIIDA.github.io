53948af3168a04cea9fe3a91c93ddadbe134081d
Naohiro IIDA <nao@nekotorobot.com>
2017-04-29 14:21:05 +0900

    Update junction.js

--- junction.js
+++ junction.js
@@ -1,27 +1,26 @@
 
 
 var milkcocoa = new MilkCocoa('applej226hj6o.mlkcca.com');
-var StatusStore = milkcocoa.dataStore("junction");
 
 function command_send1(btnNo){
 
   
   if (btnNo == 1){
-   StatusStore.send({ v : 1});
+    milkcocoa.dataStore('junction').send({ v : 1});
   }
 
   if (btnNo == 2){
    
-   StatusStore.send({ v : 2});
+    milkcocoa.dataStore('junction').send({ v : 1});
   }
 
   if (btnNo == 3){
    
-   StatusStore.send({ v : 3});
+    milkcocoa.dataStore('junction').send({ v : 1});
   }
    if (btnNo == 4){
    
-   StatusStore.send({ v : 4});
+    milkcocoa.dataStore('junction').send({ v : 1});
   }  
   
 }
