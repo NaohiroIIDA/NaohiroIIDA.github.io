7fa8f219a5cbfa5d040e2d9e7bec47c1b78adb88
Naohiro IIDA <nao@nekotorobot.com>
2017-04-29 10:02:45 +0900

    Update junction.js

--- junction.js
+++ junction.js
@@ -7,18 +7,20 @@ function command_send(btnNo){
 
   
   if (btnNo == 1){
-   StatusStore.send({ 'cmd' : 'R/' });
+   StatusStore.send({ V : 0 });
   }
 
   if (btnNo == 2){
-   StatusStore.send({ 'cmd' : 's/' });
+   StatusStore.send({ V : 1 });
   }
 
-  if (btnNo == 3){
-   StatusStore.send({ 'cmd' : 'q/' });
+  if (btnNo == 3){   
+    StatusStore.send({ V : 3 });
+
   }
-   if (btnNo == 4){
-   StatusStore.send({ 'cmd' : 'P180/' });
+   if (btnNo == 4){   
+     StatusStore.send({ V : 4 });
+
   }  
   
 }
